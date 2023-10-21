d3e026573598e7b6842082193cc702508b3a4e49
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2023-10-21 13:14:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.text('Hello World')
 streamlit.text('How are you?')
 
 streamlit.header("Fruityvice Fruit Advice!")
+import pandas
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
