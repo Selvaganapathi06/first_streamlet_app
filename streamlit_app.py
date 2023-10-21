52861bfaaa49639b3bed3c2c94e759a999522c10
Selvaganapathi Sennan <34333910+Selvaganapathi06@users.noreply.github.com>
2023-10-21 13:11:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ streamlit.title('Hello Selva')
 streamlit.header('Hello')
 streamlit.text('Hello World')
 streamlit.text('How are you?')
+
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
